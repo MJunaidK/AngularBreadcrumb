c68806c113b41ce063ec98393aadf3ed7be8ed60
MJunaidK <junaidcts@gmail.com>
2018-01-01 21:26:17 -0500

    Angular Gulp basic application.

---
+++ src/js/app.js
@@ -0,0 +1,7 @@
+angular.module("app",['ngRoute'])
+.config(['$routeProvider',function($routeProvider){
+	$routeProvider.when('/',{
+		templateUrl: 'views/home.html',
+		controller:'HomeCtrl'
+	})
+}])
---
+++ src/js/controllers/HomeCtrl.js
@@ -0,0 +1,4 @@
+angular.module('app')
+.controller('HomeCtrl',['$scope',function($scope){
+	$scope.message="God is Great.Bless me God!!!";
+}])
